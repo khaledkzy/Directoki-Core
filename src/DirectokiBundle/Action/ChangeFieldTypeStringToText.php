d5be800daad6b0870325fe9ba5d51e35efbb76cd
James Baster <james@jarofgreen.co.uk>
2017-04-23 07:59:43 +0100

    Change Field Type - String to Text

---
+++ src/DirectokiBundle/Action/ChangeFieldTypeStringToText.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace DirectokiBundle\Action;
+
+use DirectokiBundle\Entity\Field;
+use DirectokiBundle\Entity\RecordHasFieldStringValue;
+use DirectokiBundle\Entity\RecordHasFieldTextValue;
+use DirectokiBundle\FieldType\FieldTypeText;
+
+/**
+ *  @license 3-clause BSD
+ *  @link https://github.com/Directoki/Directoki-Core/blob/master/LICENSE.txt
+ */
+class ChangeFieldTypeStringToText
+{
+
+    protected $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function change(Field $field) {
+
+        $doctrine = $this->container->get('doctrine')->getManager();
+
+        $field->setFieldType(FieldTypeText::FIELD_TYPE_INTERNAL);
+        $doctrine->persist($field);
+        $doctrine->flush($field);
+
+        /** @var RecordHasFieldStringValue $recordHasFieldStringValue */
+        foreach($doctrine->getRepository('DirectokiBundle:RecordHasFieldStringValue')->findByField($field) as $recordHasFieldStringValue) {
+
+            $recordHasFieldTextValue = new RecordHasFieldTextValue();
+            $recordHasFieldTextValue->setValue($recordHasFieldStringValue->getValue());
+            $recordHasFieldTextValue->setLocale($recordHasFieldStringValue->getLocale());
+            $recordHasFieldTextValue->setField($recordHasFieldStringValue->getField());
+            $recordHasFieldTextValue->setRecord($recordHasFieldStringValue->getRecord());
+            $recordHasFieldTextValue->setCreatedAt($recordHasFieldStringValue->getCreatedAt());
+            $recordHasFieldTextValue->setCreationEvent($recordHasFieldStringValue->getCreationEvent());
+            $recordHasFieldTextValue->setApprovedAt($recordHasFieldStringValue->getApprovedAt());
+            $recordHasFieldTextValue->setApprovalEvent($recordHasFieldStringValue->getApprovalEvent());
+            $recordHasFieldTextValue->setRefusedAt($recordHasFieldStringValue->getRefusedAt());
+            $recordHasFieldTextValue->setRefusalEvent($recordHasFieldStringValue->getRefusalEvent());
+
+            $doctrine->persist($recordHasFieldTextValue);
+            $doctrine->remove($recordHasFieldStringValue);
+            $doctrine->flush(array($recordHasFieldStringValue,$recordHasFieldTextValue));
+
+        }
+
+    }
+
+}
---
+++ src/DirectokiBundle/Command/ChangeFieldTypeStringToTextCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace DirectokiBundle\Command;
+
+use DirectokiBundle\Action\ChangeFieldTypeStringToText;
+use DirectokiBundle\Entity\Record;
+use DirectokiBundle\Entity\RecordHasState;
+use DirectokiBundle\FieldType\FieldTypeString;
+use DirectokiBundle\FieldType\FieldTypeText;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ *  @license 3-clause BSD
+ *  @link https://github.com/Directoki/Directoki-Core/blob/master/LICENSE.txt
+ */
+class ChangeFieldTypeStringToTextCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('directoki:change-field-type-string-to-text')
+            ->setDescription('Change Field Type - String to Text.')
+            ->setHelp('This command allows you toChange Field Type - String to Text.')
+            ->addArgument('project', InputArgument::REQUIRED, 'Project')
+            ->addArgument('directory', InputArgument::REQUIRED, 'Directory')
+            ->addArgument('field', InputArgument::REQUIRED, 'Field');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+
+        $doctrine = $this->getContainer()->get('doctrine')->getManager();
+
+        $project = $doctrine->getRepository('DirectokiBundle:Project')->findOneByPublicId($input->getArgument('project'));
+        if (!$project) {
+            $output->writeln('Cant load Project');
+            return;
+        }
+        $output->writeln('Project: '. $project->getTitle());
+
+        $directory = $doctrine->getRepository('DirectokiBundle:Directory')->findOneBy(array('publicId'=>$input->getArgument('directory'),'project'=>$project));
+        if (!$directory) {
+            $output->writeln('Cant load Directory');
+            return;
+        }
+        $output->writeln('Directory: '. $directory->getTitleSingular());
+
+        $field = $doctrine->getRepository('DirectokiBundle:Field')->findOneByPublicId(array('publicId'=>$input->getArgument('field'),'directory'=>$directory));
+        if (!$field) {
+            $output->writeln('Cant load Field');
+            return;
+        }
+        $output->writeln('Field: '. $field->getTitle());
+
+        // We also allow Text because a change might have stopped half way through and we want people to be able to run it again.
+        if ($field->getFieldType() != FieldTypeString::FIELD_TYPE_INTERNAL && $field->getFieldType() != FieldTypeText::FIELD_TYPE_INTERNAL) {
+            $output->writeln('Field is not a Text Or String!');
+            return;
+        }
+
+        $action = new ChangeFieldTypeStringToText($this->getContainer());
+        $action->change($field);
+
+        $output->writeln('Done.');
+
+    }
+
+}
